cf4f74eb5b3a4c6636e8919d4417ebbacb359448
Urusa <usaiyed.9127@gmail.com>
2021-04-16 17:31:36 -0600

    created database in mongodb

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+
+//Middlewares - a function that executes
+//whenever a route is hit.
+// app.use('/posts', () => {
+// 	console.log('This is a middleware running');
+// });
 
 //Routes
 app.get('/', (req,res) => {
@@ -10,6 +16,10 @@ app.get('/', (req,res) => {
 app.get('/posts', (req,res) => {
 	res.send('We are on posts');
 })
+
+
+//Connect to db
+//mongoose.connect();
 
 //How do we start listening to the server
 app.listen(3000);
